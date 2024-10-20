ea9cfeb79fa700a0a0baeb6f70fa0507062cf456
jpablopachar <jpablopachar1993@gmail.com>
2024-10-20 00:58:07 -0500

    Chat para admin

--- src/app/views/admin/chat-seller.component.ts
+++ src/app/views/admin/chat-seller.component.ts
@@ -1,13 +1,32 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  ViewChild,
+  WritableSignal,
+  inject,
+  signal
+} from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { SocketService } from '@app/services'
+import { Store } from '@ngrx/store'
 
 @Component({
   selector: 'app-chat-seller',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  template: `<p>chat-seller works!</p>`,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './chat-seller.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ChatSellerComponent { }
+export class ChatSellerComponent {
+  @ViewChild('scrollRef') scrollRef!: ElementRef | undefined;
+
+  private readonly _store = inject(Store);
+  private readonly _cdr: ChangeDetectorRef = inject(ChangeDetectorRef);
+  private readonly _socketService: SocketService = inject(SocketService);
+
+  public $text: WritableSignal<string> = signal('');
+  public $receiverMessage: WritableSignal<string> = signal('');
+}
