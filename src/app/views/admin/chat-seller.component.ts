8c5e63843e3e2e2eb8a136aec2ef96b27335c09c
jpablopachar <jpablopachar1993@gmail.com>
2024-11-01 23:55:01 -0500

    Se escuchan cambios en signals

--- src/app/views/admin/chat-seller.component.ts
+++ src/app/views/admin/chat-seller.component.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { CommonModule } from '@angular/common'
 import {
   ChangeDetectionStrategy,
@@ -8,10 +10,12 @@ import {
   Signal,
   ViewChild,
   WritableSignal,
+  effect,
   inject,
-  signal
+  signal,
 } from '@angular/core'
 import { FormsModule } from '@angular/forms'
+import { ActivatedRoute } from '@angular/router'
 import { InfoUser } from '@app/models'
 import { SocketService } from '@app/services'
 import {
@@ -23,6 +27,7 @@ import {
   selectSuccessMessage,
 } from '@app/store/chat'
 import { Store } from '@ngrx/store'
+import { ToastrService } from 'ngx-toastr'
 
 @Component({
   selector: 'app-chat-seller',
@@ -36,6 +41,8 @@ export class ChatSellerComponent implements OnInit {
 
   private readonly _store = inject(Store);
   private readonly _cdr: ChangeDetectorRef = inject(ChangeDetectorRef);
+  private readonly _route: ActivatedRoute = inject(ActivatedRoute);
+  private readonly _toastr: ToastrService = inject(ToastrService);
   private readonly _socketService: SocketService = inject(SocketService);
 
   public $sellers: Signal<InfoUser[]> = this._store.selectSignal(selectSellers);
@@ -46,10 +53,62 @@ export class ChatSellerComponent implements OnInit {
   public $currentSeller = this._store.selectSignal(selectCurrentSeller);
   public $SuccessMessage = this._store.selectSignal(selectSuccessMessage);
 
+  private $_receiverMessage: WritableSignal<string | any> = signal('');
+
   public $text: WritableSignal<string> = signal('');
-  public $receiverMessage: WritableSignal<string> = signal('');
+  public $sellerId: WritableSignal<string | null> = signal(null);
+
+  constructor() {
+    effect(
+      (): void => {
+        if (this.$sellerId()) {
+          // Get admin messages
+        }
+
+        if (this.$SuccessMessage()) {
+          this._socketService.emit(
+            'sendMessageAdminToSeller',
+            this.$sellerAdminMessage()[this.$sellerAdminMessage().length - 1]
+          );
+
+          this._store.dispatch(chatActions.messageClear());
+        }
+
+        if (this.$_receiverMessage()) {
+          if (this.$_receiverMessage()) {
+            if (this.$_receiverMessage().senderId === this.$sellerId() && this.$_receiverMessage().receiverId === '') {
+              this._store.dispatch(chatActions.updateSellerMessage(this.$_receiverMessage()));
+            } else {
+              this._toastr.success(`${this.$_receiverMessage().senderName} send a message`);
+
+              this._store.dispatch(chatActions.messageClear());
+            }
+          }
+        }
+
+        if (this.$sellerAdminMessage()) {
+          this._cdr.detectChanges();
+
+          requestAnimationFrame((): void => {
+            if (this.scrollRef) {
+              const elementTemp = this.scrollRef.nativeElement;
+
+              elementTemp.scrollTop = elementTemp.scrollHeight;
+            }
+          });
+        }
+      },
+      { allowSignalWrites: true }
+    );
+  }
 
   ngOnInit(): void {
+    this.$sellerId.set(this._route.snapshot.params['sellerId']);
+
+    this._socketService.on('', (message) => {
+      this.$_receiverMessage.set(message);
+    })
+
     this._store.dispatch(chatActions.getSellers());
   }
 }
